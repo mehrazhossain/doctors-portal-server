554d51c1d2166d686023d917bc1088198f7f796e
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-15 01:09:08 +0600

    Limit one booking per user per treatment per day

--- index.js
+++ index.js
@@ -52,8 +52,13 @@ async function run() {
         date: booking.date,
         patient: booking.patient,
       };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+
       const result = await bookingCollection.insertOne(booking);
-      res.send(result);
+      res.send({ success: true, result });
     });
   } finally {
   }
