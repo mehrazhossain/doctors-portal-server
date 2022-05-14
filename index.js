40b4547c025abd3d7b560c30a7e529fe7f33a7b9
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-15 02:55:51 +0600

    Find available time slots for a day

--- index.js
+++ index.js
@@ -35,6 +35,30 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+
+    app.get('/available', async (req, res) => {
+      const date = req.query.date || 'May 15, 2022';
+      console.log(date);
+
+      // step 1: get all services
+      const services = await serviceCollection.find().toArray();
+
+      // step 2: get the booking of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3: for each service, find bookings for that service
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (b) => b.treatment === service.name
+        );
+        const booked = serviceBookings.map((s) => s.slot);
+        const available = service.slots.filter((s) => !booked.includes(s));
+        service.available = available;
+      });
+
+      res.send(services);
+    });
     /**
      * API Naming Convention
      * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
