bc401c3b531ff5269d3787c592610210a4d4c560
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-17 00:00:36 +0600

    setup get api for all users

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
       res.send(services);
     });
 
+    // get all users
+    app.get('/user', async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
