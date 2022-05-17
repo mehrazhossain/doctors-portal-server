e918f5cb6db61ab414cbdf2b882046422b3f6401
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-17 09:52:16 +0600

    create a message for server

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Doctors Portal Server is Running');
 });
 
 app.listen(port, () => {
