d4e927c82bb28afb587f2e9d16ddfeb9c37bd49e
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-19 01:03:23 +0600

    Set email after appointment confirmation

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 
 // middleware
 app.use(cors());
@@ -32,6 +34,45 @@ function verifyJWT(req, res, next) {
     }
     req.decoded = decoded;
     next();
+  });
+}
+
+const emailSenderOptions = {
+  auth: {
+    api_key: process.env.EMAIL_SENDER_KEY,
+  },
+};
+
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking) {
+  const { patient, patientName, treatment, date, slot } = booking;
+
+  var email = {
+    from: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    html: ` 
+    <div>
+        <p> Hello ${patientName}, </p>
+        <h3>Your Appointment for ${treatment} is confirmed</h3>
+        <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+
+        <h3>Our Address</h3>
+        <p>Banani, Road #11, Block #C House #105.</p>
+        <p>Dhaka, Bangladesh.</p>
+        <a href="https:google.com">unsubscribe</a>
+    </div>
+    `,
+  };
+
+  emailClient.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Message sent: ', +info);
+    }
   });
 }
 
@@ -188,6 +229,7 @@ async function run() {
       }
 
       const result = await bookingCollection.insertOne(booking);
+      sendAppointmentEmail(booking);
       res.send({ success: true, result });
     });
 
