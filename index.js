adb1ea354cf23feb7149aeddb677bd0f791411b4
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-20 08:33:31 +0600

    Create Payment route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 var nodemailer = require('nodemailer');
@@ -212,6 +212,13 @@ async function run() {
       } else {
         return res.status(403).send({ message: 'forbidden access' });
       }
+    });
+
+    app.get('/booking/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
     });
 
     // add a new booking
