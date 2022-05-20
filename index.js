80ce2ac6325f510ac13f4541296da702264a6a45
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-21 00:15:36 +0600

    Server side payment intents API to get client Secret from stripe

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(cors());
@@ -105,6 +106,19 @@ async function run() {
         res.status(403).send({ message: 'forbidden' });
       }
     };
+
+    // stripe payments
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
 
     app.get('/service', async (req, res) => {
       const query = {};
