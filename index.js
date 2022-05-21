9b3bbbdba250fa262ff4840115f8e2a150ae129e
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-21 09:48:47 +0600

    payment confirmation email outline created

--- index.js
+++ index.js
@@ -76,6 +76,38 @@ function sendAppointmentEmail(booking) {
     }
   });
 }
+function sendPaymentConfirmationEmail(booking) {
+  const { patient, patientName, treatment, date, slot } = booking;
+
+  var email = {
+    from: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `We have received your payment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    text: `Your payment for this Appointment ${treatment} is on ${date} at ${slot} is Confirmed`,
+    html: ` 
+    <div>
+        <p> Hello ${patientName}, </p>
+        <h3>Thank you for your payment.</h3>
+        <h3>We have received your payment</h3>
+
+        <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+
+        <h3>Our Address</h3>
+        <p>Banani, Road #11, Block #C House #105.</p>
+        <p>Dhaka, Bangladesh.</p>
+        <a href="https:google.com">unsubscribe</a>
+    </div>
+    `,
+  };
+
+  emailClient.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Message sent: ', +info);
+    }
+  });
+}
 
 async function run() {
   try {
